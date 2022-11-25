60ff50ce318719a7e22de5b1661d07b4c1a7c018
Arko <arkoroyb@gmail.com>
2022-11-26 02:33:44 +0600

    advertise a item can be done

--- src/Components/Pages/Seller/MyProducts/MyProducts.js
+++ src/Components/Pages/Seller/MyProducts/MyProducts.js
@@ -17,6 +17,21 @@ const MyProducts = () => {
         }
     })
 
+    const handleAdvertise = (id) => {
+        fetch(`http://localhost:5000/advertisement?id=${id}`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
+        .then(res => res.json())
+        .then(data => {
+            alert('successfully added to the advertisement')
+        })
+    
+        
+    }
+
     if (isLoading) {
         <Loading></Loading>
     }
@@ -33,6 +48,7 @@ const MyProducts = () => {
                             <div className="card-body">
                                 <h2 className="card-title">{fridge.name}</h2>
                                 <small>{ }</small>
+                                <h6 className="text-md">Stock: {fridge.stock}</h6>
                                 <h4 className="text-xl">Resale Price: {fridge.resalePrice} TK</h4>
                                 <h4 className="text-xl">Original Price: {fridge.originalPrice} TK</h4>
                                 <h5 className="text-lg">Used: {fridge.yearUse} years</h5>
@@ -41,7 +57,7 @@ const MyProducts = () => {
                                 <p>Post Time: {fridge.post}</p>
                                 <div className="flex justify-between">
                                     <div className="card-actions justify-start">
-                                        <button className="btn btn-primary">Add WishList</button>
+                                        <button onClick={()=>handleAdvertise(fridge._id)} className="btn btn-primary">Advertise</button>
                                     </div>
                                     <div className="card-actions justify-end">
                                         <button className="btn btn-primary">Book Now</button>
