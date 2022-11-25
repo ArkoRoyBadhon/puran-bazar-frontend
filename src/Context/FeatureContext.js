97bc54da312325e170286dfadedfb3731a29a192
Arko <arkoroyb@gmail.com>
2022-11-25 18:44:45 +0600

    Now all issue fixed with dark mode

--- src/Context/FeatureContext.js
+++ src/Context/FeatureContext.js
@@ -3,21 +3,21 @@ import React, { createContext, useEffect, useState } from 'react';
 export const otherContext = createContext();
 
 const FeatureContext = ({ children }) => {
-    const [iconValue, setIconValue] = useState("Moon")
-    const [theme, setTheme] = useState("dark")
+    const [iconValue, setIconValue] = useState("sun")
+    const [theme, setTheme] = useState("light")
 
 
     const handleClick = (event) => {
         if (iconValue === 'Moon') {
             setIconValue("Sun")
             localStorage.setItem('theme', 'light');
-            
+
         } else {
             setIconValue('Moon')
             localStorage.setItem('theme', 'dark');
-            
+
         }
-        
+
 
     }
 
